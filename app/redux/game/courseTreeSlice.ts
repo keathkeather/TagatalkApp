737f45a8cb7137638df14bcb8988c0dff56616b5
shaniacc <canoy.shania@gmail.com>
2024-10-07 21:00:25 +0800

    [SHANIA][UPDATE] Necessary changes for course redux

--- app/redux/game/courseTreeSlice.ts
+++ app/redux/game/courseTreeSlice.ts
@@ -1,10 +1,47 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { WritableDraft } from "immer";
 import { getCourseTree } from "~/components/courseTree";
+
+export interface FileAsset {
+    assetId: string,
+    assetClassifier: string,
+    assetType: string,
+    isCorrectAnswer: boolean,
+    fileUrl: string,
+}
+
+export interface TextAsset {
+    assetId: string,
+    assetClassifier: string,
+    assetType: string,
+    isCorrectAnswer: boolean,
+    textContent: string,
+}
+
+export interface GameAsset {
+    gameId: string,
+    textAssets: TextAsset[],
+    fileAssets: FileAsset[],
+}
+
+interface Game {
+    id: any;
+    gameId: string;
+    gameType: number; // 'Type 1', 'Type 2', 'Type 3'
+    gameValue: number; // score
+    gameSkill: string; // 'reading', 'writing', 'listening', 'speaking'
+    gameUnit: string; // Unit Description
+    gameUnitNumber: number; // Unit Number
+    gameLessonNumber: number; // Lesson Number
+    gameLesson: string; // Lesson Title
+    gameAssets: GameAsset[];
+}
 
 interface Lesson {
     id: string;
     lessonNumber: number;
     lessonName: string;
+    game: Game[];
 }
 
 interface Unit {
@@ -18,13 +55,23 @@ interface CourseTreeState {
 }
 
 const initialState: CourseTreeState = {
-    course: []
-}
+    course: [],
+};
 
 export const handleCourseTree = createAsyncThunk('courseTree/getCourseTree', async (gameSkill: string) => {
     const courseTree = await getCourseTree(gameSkill);
     return courseTree;
 });
+
+// export const handleGameAsset = createAsyncThunk('courseTree/getGameAssets', async (gameId: string) => {
+//     const gameAsset = await getGameAssets(gameId);
+//     return gameAsset;
+// });
+
+// export const handleGame = createAsyncThunk('courseTree/getGame', async (gameLessonNumber: number) => {
+//     const game = await getGame(gameLessonNumber);
+//     return game;
+// });
 
 const courseTreeSlice = createSlice({
     name: "courseTreeState",
@@ -33,9 +80,10 @@ const courseTreeSlice = createSlice({
     extraReducers: (builder) => {
         builder
             .addCase(handleCourseTree.fulfilled, (state, action) => {
-                console.log('Fulfilled Action Payload:', action.payload); // Log the payload
+                console.log('Fulfilled Action Payload:', JSON.stringify(action.payload, null, 2)); // Log the payload (Pretty) like in postman
+                // console.log('Fulfilled Action Payload:');
                 if (action.payload) {
-                    state.course = action.payload.course;
+                    state.course = action.payload.course as unknown as WritableDraft<Unit>[];
                 }
             })
             .addCase(handleCourseTree.rejected, (state, action) => {
